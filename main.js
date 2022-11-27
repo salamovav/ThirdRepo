cb6899c055ac17c0a216e9e70f0eedf7d7229894
Tora <109001487+salamovav@users.noreply.github.com>
2022-11-27 19:10:27 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+// document.getElementById('first').innerHTML="Hello world!";
+// document.getElementById('second').innerHTML="<h1> Hello world! </h1>";
+// let name="Меня зовут Виктория";
+// document.getElementById('first').innerHTML= name;
+// document.querySelector('.second').innerHTML="Hello!";
+// document.querySelector('p').innerHTML="Желаю хорошего дня!";
+
+// let inputIn= document.querySelector('.text');
+// let btn=document.querySelector('.btn');
+// let div=document.querySelector('.out');
+
+// btn.onclick=function(){
+//     console.log('Da');
+//     console.log(inputIn.value);
+//     let e= +inputIn.value;
+//     console.log(e);
+//     div.innerHTML=e;
+// }
+
+// let myBirthDay= prompt('Когда у тебя др?')
+// console.log(myBirthDay);
+
+// if(myBirthDay < 22) {
+//     alert('Да ты молод бро!');
+// }else{
+//     alert('фу старик');
+// }
+
+let whatNumber=prompt('Какое число?')
+
+if (whatNumber>100){
+    alert('Число больше 100');
+}else {
+    console.log('Число меньше 100');
+}
